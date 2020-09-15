919f8620fe740d852a33c33f89f42df53ac5cc2f
niel98 <mosesdaniel201798@gmail.com>
2020-09-15 08:45:25 +0000

    Testing adding a user

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,11 +1,26 @@
 const express = require('express')
 
+let users = []
+
 const routes = express.Router({
     mergeParams: true
 })
 
+//
 routes.get('/', (req, res) => {
     res.status(200).json({ success: true, msg: 'Landing!' })
+})
+
+//Add a user
+routes.post('/', (req, res) => {
+    try {
+        const user = req.body
+
+        const newUser = users.push(...user)
+        res.status(201).json({ success: true, data: newUser })
+    } catch (err) {
+        console.log(err.message)
+    }
 })
 
 module.exports = routes
